7a04480e87783e5ee385795469c6171315ddb05c
ujang85 <root@muhammads-MacBook-Pro.local>
2022-05-24 09:56:05 +0700

    cetak aduan

--- backend/views/aduan-presensi/index.php
+++ backend/views/aduan-presensi/index.php
@@ -5,6 +5,7 @@ use yii\bootstrap4\Modal;
 use kartik\grid\GridView;
 use hoaaah\ajaxcrud\CrudAsset; 
 use hoaaah\ajaxcrud\BulkButtonWidget;
+use kartik\export\ExportMenu;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\AduanPresensiSearch */
@@ -16,7 +17,36 @@ $this->params['breadcrumbs'][] = $this->title;
 CrudAsset::register($this);
 
 ?>
-    
+
+<?=
+
+                    'Export Data',
+                    Html::a('<i class="glyphicon glyphicon-repeat"></i>', [''],
+                    ['data-pjax'=>1, 'class'=>'btn btn-default', 'title'=>'Reset Grid']).
+                   //  '{toggleData}'.
+             
+            ExportMenu::widget([
+                        'dataProvider' => $dataProvider,
+                        'columns' => require(__DIR__.'/_columns.php'),
+                        'contentBefore' => [
+                                [
+                                'value' => $this->title,
+                                ]
+                                ],
+                        'filename' => $this->title,
+                        'exportConfig' => [
+                                    ExportMenu::FORMAT_HTML => false,
+                                    ExportMenu::FORMAT_PDF => false,
+                                    ExportMenu::FORMAT_EXCEL => false,
+                                    ExportMenu::FORMAT_TEXT => false,
+                                    ExportMenu::FORMAT_CSV =>false,
+                                    ExportMenu::FORMAT_EXCEL_X  => [
+                                     'label' => 'File Excel',
+                                ],
+                                 ],
+
+                    ])
+        ?>
 <div class="aduan-presensi-index">
     <div id="ajaxCrudDatatable">
         <?=GridView::widget([
